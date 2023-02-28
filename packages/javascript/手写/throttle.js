66100dd3caef4e27d3a36467e05ff8812237f2cd
tisou1 <57515396+tisou1@users.noreply.github.com>
2023-02-28 13:56:37 +0800

    chore: 节流函数,时间戳版本

--- packages/javascript/手写/throttle.js
+++ packages/javascript/手写/throttle.js
@@ -12,10 +12,25 @@ export const throttle = (fn, delay) => {
     if (!timer) {
       fn(...args)
       timer = setTimeout(() => {
-        // 当函数体执行的时候,将timer置位null
+        // 当函数体执行的时候,将timer置为null
         clearTimeout(timer)
         timer = null
       }, delay)
+    }
+  }
+}
+
+// 时间戳版本
+export const throttle2 = (fn, delay) => {
+  let preDate = 0
+
+  return (...args) => {
+    let nowDate = Date.now()
+
+    if(nowDate - preDate > delay) {
+      // 如果时间差大于设置的阈值delay,则执行fn
+      preDate = nowDate
+      fn(args)
     }
   }
 }
