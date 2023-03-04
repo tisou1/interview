11f9a1721fd8a6274acea5f614755f7cb5931b63
tisou1 <tisou1@qq.com>
2023-03-04 10:30:09 +0800

    chore: call bind apply测试case

--- packages/javascript/手写/instanceof.js
+++ packages/javascript/手写/instanceof.js
@@ -1,26 +1,25 @@
 /**
- * 
- * @param {*} instance 
- * @param {*} target 
+ *
+ * @param {*} instance
+ * @param {*} target
  * @description 实现instanceOf,检测构造函数的 prototype 属性是否出现在某个实例对象的原型链上。
  */
 
+const _instanceof = (instance, _constructor) => {
+  // 由于要检测的是object
+  if (typeof instance !== "object" || instance === null) return false
 
-const  _instanceof = (instance, _constructor) => {
-    // 由于要检测的是object
-    if(typeof instance !== 'object' || instance === null) return
-
-    // 获取当前实例对象的原型
-    let proto = Object.getPrototypeOf(instance)
-    while(proto) {
-        // 实例原型 等于 构造函数的原型
-        if (proto === _constructor.prototype) {
-            return true
-        }
-        proto = Object.getPrototypeOf(proto)
+  // 获取当前实例对象的原型
+  let proto = Object.getPrototypeOf(instance)
+  while (proto) {
+    // 实例原型 等于 构造函数的原型
+    if (proto === _constructor.prototype) {
+      return true
     }
+    proto = Object.getPrototypeOf(proto)
+  }
 
-    return false
+  return false
 }
 
 export default _instanceof
--- packages/javascript/手写/test/index.test.js
+++ packages/javascript/手写/test/index.test.js
@@ -2,8 +2,12 @@ import { describe, it, expect } from "vitest"
 import mytypeof from "../类型判断"
 import { unique1, unique2, unique3, unique4 } from "../unique"
 import { flat1, flat2, flat3 } from "../flat"
-import _instanceof from '../instanceof'
-import _new from '../new'
+import _instanceof from "../instanceof"
+import _new from "../new"
+
+import call from "../call"
+import apply from "../apply"
+import bind from "../bind"
 
 describe("手写测试", () => {
   it("类型判断typeof", () => {
@@ -65,27 +69,40 @@ describe("数组扁平化", () => {
   })
 })
 
-
 it("原型判断, instanceOf", () => {
   expect(_instanceof([], Array)).toBe(true)
-  expect(_instanceof('asd', Object)).toBe(true)
-  expect(_instanceof('asd', Function)).toBe(false)
+  expect(_instanceof("asd", Object)).toBe(false)
+  expect(_instanceof("asd", Function)).toBe(false)
 })
-
 
 it("new运算符", () => {
   // 用法
-function Person(name, age) {
-  this.name = name;
-  this.age = age;
+  function Person(name, age) {
+    this.name = name
+    this.age = age
 
- // 如果构造函数内部，return 一个引用类型的对象，则整个构造函数失效，而是返回这个引用类型的对象，而不是返回this
-  // 在实例中就没法获取Person原型上的getName方法
-}
-Person.prototype.say = function() {
-  console.log(this.age);
-};
-let p1 =  _new(Person, 'porty', 18).name
+    // 如果构造函数内部，return 一个引用类型的对象，则整个构造函数失效，而是返回这个引用类型的对象，而不是返回this
+    // 在实例中就没法获取Person原型上的getName方法
+  }
+  Person.prototype.say = function () {
+    console.log(this.age)
+  }
+  let p1 = _new(Person, "porty", 18)
+  expect(p1.name).toBe("porty")
+})
 
-expect(p1.name).toBe('porty')
+describe("call, apply bind 测试", () => {
+  function getName(...args) {
+    console.log("args:", args)
+    return this.name
+  }
+
+  let obj = {
+    name: "siry",
+  }
+  it("call", () => {
+    expect(call(obj, getName, "cc", "dd")).toBe("siry")
+    expect(apply(obj, getName, ["cc", "dd"])).toBe("siry")
+    expect(bind(obj, getName, "cc", "dd")("eee")).toBe("siry")
+  })
 })
