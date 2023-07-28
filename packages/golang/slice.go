98d6a66d138636d6e3ebb0dd7d18d3b43b738345
tisou1 <57515396+tisou1@users.noreply.github.com>
2023-07-28 14:25:30 +0800

    feat: add FilterMap

--- packages/golang/slice.go
+++ packages/golang/slice.go
@@ -13,6 +13,20 @@ func Filter[V any](collection []V, predicate func(item V, index int) bool) []V {
 	return result
 }
 
+// 过滤加map
+func FilterMap[T any, R any](collection []T, callback func(item T , index int) (R, bool)) []R {
+	// 声明一个R类型的空切片,  也可用make   make([]R, 0, len(collection))
+	result := []R{}
+
+	for i, item := range collection {
+		if r, ok := callback(item, i); ok {
+			result = append(result, r)
+		}
+	}
+
+	return result
+}
+
 
 func Map[T any, R any](collection []T, iteratee func(item T, index int) V) []R {
 	result := make([]R, len(collection))
@@ -26,7 +40,7 @@ func Map[T any, R any](collection []T, iteratee func(item T, index int) V) []R {
 }
 
 
-func Reduce[T any R any](collection []T, accumulator func(agg R, item T, index int) R, initial R) R {
+func Reduce[T any, R any](collection []T, accumulator func(agg R, item T, index int) R, initial R) R {
 	// 也可以直接使用initial
 	tempAgg := initial
 	for i, item := range collection {
@@ -35,3 +49,10 @@ func Reduce[T any R any](collection []T, accumulator func(agg R, item T, index i
 
 	return tempAgg
 }
+
+func ForEach[T any](collection T, iteratee func(item T, index int)) {
+	for i, item := range collection {
+		iteratee(item, i)
+	}
+}
+
