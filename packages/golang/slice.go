10a287dc820b06e885bacca6842f59766238e115
tisou1 <57515396+tisou1@users.noreply.github.com>
2023-08-24 19:19:23 +0800

    feat: add UniqBy

--- packages/golang/slice.go
+++ packages/golang/slice.go
@@ -73,3 +73,21 @@ func Uniq[T comparable](collection []T) []T {
 	
 	return result
 }
+
+
+func UniqBy[T any, U comparable](collection []T, iteratee func(item T) U) []T {
+	result := make([]T, 0 len(collection))
+	seen := make(map[U]struct{}, len(collection))
+
+	for _, item := range collection {
+		key := iteratee(item)
+
+		if _, ok := seen[key], ok {
+			continue
+		}
+		seen[key] = struct{}{}
+		result = append(result, item)
+	}
+
+	return result
+}
